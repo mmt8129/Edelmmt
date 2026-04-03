bf19236fc3a4e9e1553b9a988d596242eb25c0dd
mmt8129 <06mmt8129@gmail.com>
2026-04-03 17:39:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,40 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = "edel-v1";
+const urlsToCache = [
+  "/Edelmmt/",
+  "/Edelmmt/index.html",
+  "/Edelmmt/tezgah.html",
+  "/Edelmmt/depo.html",
+  "/Edelmmt/hespmak.html",
+  "/Edelmmt/notlar.html",
+  "/Edelmmt/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
