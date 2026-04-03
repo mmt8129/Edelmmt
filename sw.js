4775c3bf031d3101e55fd0bb926976e06725916b
mmt8129 <06mmt8129@gmail.com>
2026-04-03 12:37:14 +0300

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "edel-v1";
+const urlsToCache = [
+  "/edel/",
+  "/edel/index.html",
+  "/edel/tezgah.html",
+  "/edel/depo.html",
+  "/edel/hespmak.html",
+  "/edel/notlar.html",
+  "/edel/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
