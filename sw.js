9774fc3e5b4e04280ba0b50e89fe5cb5ee28f212
mmt8129 <06mmt8129@gmail.com>
2026-04-03 17:43:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
-const CACHE_NAME = "edel-v1";
+const CACHE_NAME = "edel-pwa-v1";
 const urlsToCache = [
   "/Edelmmt/",
   "/Edelmmt/index.html",
   "/Edelmmt/tezgah.html",
   "/Edelmmt/depo.html",
-  "/Edelmmt/hespmak.html",
+  "/Edelmmt/hesap.html",
   "/Edelmmt/notlar.html",
-  "/Edelmmt/manifest.json"
+  "/Edelmmt/manifest.json",
+  "/Edelmmt/icon-196.png",
+  "/Edelmmt/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
   self.skipWaiting();
 });
